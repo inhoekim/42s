8a227b747d869c0e1ee8c30cb78176aac69fdff7
inhkim <inhoe.kim95@gmail.com>
2023-03-17 22:08:00 +0900

    fix) ft_strtrim

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -12,47 +12,61 @@
 
 #include "libft.h"
 
-size_t	get_left_idx(char const *s1, char const *set)
+static size_t	get_left_idx(char const *s1, char const *set)
 {
 	size_t	left_idx;
+	size_t	set_idx;
+	size_t	s1_len;
 	size_t	set_len;
-	size_t	cnt;
 
 	left_idx = 0;
+	s1_len = ft_strlen(s1);
 	set_len = ft_strlen(set);
-	cnt = 0;
-	while (cnt < set_len)
+	while (s1_len > left_idx + set_len)
 	{
-		if (s1[left_idx] && s1[left_idx] == set[cnt])
+		set_idx = 0;
+		while (set_idx < set_len)
 		{
-			left_idx++;
-			set_idx++;
+			if (s1[left_idx++] == set[set_idx++])
+				;
+			else
+			{
+				left_idx -= set_idx;
+				s1_len = 0;
+				break;
+			}
 		}
-		set_idx++;
 	}
 	return (left_idx);
 }
 
-long long	get_right_idx(char const *s1, char const *set)
+static long long	get_right_idx(char const *s1, char const *set)
 {
 	long long	right_idx;
 	long long	set_idx;
+	long long	set_len;
 
 	right_idx = (long long)ft_strlen(s1);
-	set_idx = 0;
-	while (set[set_idx] && right_idx > 0)
+	set_len = (long long)ft_strlen(set);
+	while ((right_idx > set_len) && right_idx > 0)
 	{
-		if (s1[right_idx - 1] && s1[right_idx] == set[set_idx])
+		set_idx = 0;
+		while (set_idx < set_len)
 		{
-			right_idx--;
-			set_idx = -1;
+			if (s1[right_idx--] == set[set_idx++])
+				;
+			else
+			{
+				right_idx += set_idx;
+				set_len = right_idx + 1;
+				break;
+			}
 		}
-		set_idx++;
 	}
 	return (right_idx);
 }
 
-void	set_str(char const *s1, char *new_str, \
+static void	set_new_str(char *new_str, char const *s1, \
 long long left_idx, long long right_idx)
 {
 	long long	idx;
@@ -73,6 +87,8 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char		*new_str;
 	long long	str_size;
 
+	if (set == NULL || ft_strlen(set) == 0)
+		return (s1);
 	left_idx = get_left_idx(s1, set);
 	right_idx = get_right_idx(s1, set);
 	if (left_idx > right_idx)
@@ -84,6 +100,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 	new_str = (char *)malloc(sizeof(char) * (str_size + 1));
 	if (new_str == NULL)
 		return (NULL);
-	set_str(s1, new_str, left_idx, right_idx);
+	set_new_str(new_str, s1, left_idx, right_idx);
 	return (new_str);
 }
