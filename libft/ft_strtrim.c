27625b0b85974251ee7b5750f01e9260936df5d6
inhkim <inhoe.kim95@gmail.com>
2023-03-17 22:24:12 +0900

    fix) ft_strtrim, Fix by checking for only one element, not for all "set" elements

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -16,54 +16,53 @@ static size_t	get_left_idx(char const *s1, char const *set)
 {
 	size_t	left_idx;
 	size_t	set_idx;
-	size_t	s1_len;
 	size_t	set_len;
 
 	left_idx = 0;
-	s1_len = ft_strlen(s1);
 	set_len = ft_strlen(set);
-	while (s1_len > left_idx + set_len)
+	while (s1[left_idx])
 	{
 		set_idx = 0;
 		while (set_idx < set_len)
 		{
-			if (s1[left_idx++] == set[set_idx++])
-				;
-			else
+			if (s1[left_idx] == set[set_idx])
 			{
-				left_idx -= set_idx;
-				s1_len = 0;
+				left_idx++;
 				break;
 			}
+			else
+				set_idx++;
 		}
+		if (set_idx == set_len)
+			return (left_idx);
 	}
 	return (left_idx);
 }
 
-static long long	get_right_idx(char const *s1, char const *set)
+static size_t	get_right_idx(char const *s1, char const *set)
 {
 	long long	right_idx;
 	long long	set_idx;
 	long long	set_len;
 
-	right_idx = (long long)ft_strlen(s1);
-	set_len = (long long)ft_strlen(set);
-	while ((right_idx > set_len) && right_idx > 0)
+	right_idx = (long long)ft_strlen(s1) - 1;
+	while (right_idx > 0 && s1[right_idx])
 	{
 		set_idx = 0;
 		while (set_idx < set_len)
 		{
-			if (s1[right_idx--] == set[set_idx++])
-				;
-			else
+			if (s1[right_idx] == set[set_idx])
 			{
-				right_idx += set_idx;
-				set_len = right_idx + 1;
+				right_idx--;
 				break;
 			}
+			else
+				set_idx++;
 		}
+		if (set_idx == set_len)
+			return ((size_t)right_idx);
 	}
-	return (right_idx);
+	return ((size_t)right_idx);
 }
 
 static void	set_new_str(char *new_str, char const *s1, \
