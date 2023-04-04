005985641625924a0a57d6a00bc263914075d8fd
inhkim <inhoe.kim95@gmail.com>
2023-04-05 02:12:06 +0900

    ok

--- printf/bonus_srcs/ft_print_int_bonus.c
+++ printf/bonus_srcs/ft_print_int_bonus.c
@@ -13,7 +13,7 @@
 #include "ft_printf_bonus.h"
 #include <unistd.h>
 
-static void	init_wdata(t_check_wdata *wdata)
+static void init_wdata(t_check_wdata *wdata)
 {
 	wdata->cnt = 0;
 	wdata->is_minus = 0;
@@ -52,22 +52,22 @@ char *str, t_len_data *len_data, int is_minus)
 	if (fm.flag_ascii['-'])
 	{
 		if (is_minus)
-			ft_check_wr_err(write(1, "-", 1), &wdata);
+			ft_err(write(1, "-", 1), &wdata);
 		if (!is_minus && fm.flag_ascii['+'])
-			ft_check_wr_err(write(1, "+", 1), &wdata);
-		ft_check_wr_err(ft_print_zero(fm.prec_width - len_data->str_len), &wdata);
-		ft_check_wr_err(ft_putstr_fd(str, len_data->str_len, 1), &wdata);
-		ft_check_wr_err(ft_print_width(fm.width - len_data->actual_len), &wdata);
+			ft_err(write(1, "+", 1), &wdata);
+		ft_err(ft_print_zero(fm.prec_width - len_data->str_len), &wdata);
+		ft_err(ft_putstr_fd(str, len_data->str_len, 1), &wdata);
+		ft_err(ft_print_width(fm.width - len_data->actual_len), &wdata);
 	}
 	else
 	{
-		ft_check_wr_err(ft_print_width(fm.width - len_data->actual_len), &wdata);
+		ft_err(ft_print_width(fm.width - len_data->actual_len), &wdata);
 		if (is_minus)
-			ft_check_wr_err(write(1, "-", 1), &wdata);
+			ft_err(write(1, "-", 1), &wdata);
 		if (!is_minus && fm.flag_ascii['+'])
-			ft_check_wr_err(write(1, "+", 1) , &wdata);
-		ft_check_wr_err(ft_print_zero(fm.prec_width - len_data->str_len), &wdata);
-		ft_check_wr_err(ft_putstr_fd(str, len_data->str_len, 1), &wdata);
+			ft_err(write(1, "+", 1) , &wdata);
+		ft_err(ft_print_zero(fm.prec_width - len_data->str_len), &wdata);
+		ft_err(ft_putstr_fd(str, len_data->str_len, 1), &wdata);
 	}
 	return (wdata);
 }
